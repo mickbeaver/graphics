94fc1bafc02ba9e83e575e661980ed4d1960b51d
Mick Beaver <m.charles.beaver@gmail.com>
2016-02-17 11:24:44 -0600

    Builds on Visual Studio 2015. Added new external libs.

---
+++ external/msvc/include/check_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _CHECK_CHECK_STDINT_H
+#define _CHECK_CHECK_STDINT_H 1
+/* mickbeaver begin */
+#define HAVE_STDINT_H
+/* mickbeaver end */
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
+#endif
