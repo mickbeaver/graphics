9f62f4782f858d29901f2560abd09d2b957065a9
Mick Beaver <m.charles.beaver@gmail.com>
2016-06-15 18:21:47 -0400

    tut 01

--- gltut/tut_01_hello_triangle/tut1.c
+++ gltut/tut_01_hello_triangle/tut1.c
@@ -1,17 +1,62 @@
+#include <GLES2/gl2.h>
 #include "../framework/framework.h"
+
+static const float skVertexPositions[] = {
+    0.75f, 0.75f, 0.0f, 1.0f,
+    0.75f, -0.75f, 0.0f, 1.0f,
+    -0.75f, -0.7f, 0.0f, 1.0f
+};
+
+static GLuint sTheProgram;
+static GLuint sPositionBufferObject;
+
+static void initializeProgram();
+static void initializeVertexBuffer();
+
+void
+initializeProgram()
+{
+    
+}
+
+void
+initializeVertexBuffer()
+{
+    glGenBuffers(1, &sPositionBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, sPositionBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skVertexPositions), skVertexPositions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 
 void
 gltutDefaultSettingsInit(GltutDefaultSettings *settings)
 {
+    // empty
+    (void) settings;
 }
 
 
 void
 gltutPostRenderSystemInit(void)
 {
+    initializeProgram();
+    initializeVertexBuffer();
 }
 
 void
 gltutDisplay(void)
 {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(sTheProgram);
+
+    glBindBuffer(GL_ARRAY_BUFFER, sPositionBufferObject);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+    glUseProgram(0);
 }
