fc621ef48d678b54f836d597629704ed7ed63d15
Mick Beaver <m.charles.beaver@gmail.com>
2017-02-15 13:46:10 -0600

    Recipe for page 10 - Using the GLM library for mathematics.

---
+++ opengl_4.0_shading_language_cookbook/010-using_glm/TestGlm.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform2.hpp>
+
+
+#ifdef __cplusplus // SDL2main
+extern "C" 
+#endif
+int
+main(int argc, char** argv) {
+    (void) argc;
+    (void) argv;
+
+    glm::vec4 position = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f),
+                                 glm::vec3(0.0f, 0.0f, 0.0f),
+                                 glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(model, 90.0f, glm::vec3(0.0f, 1.0f, 0.0));
+    glm::mat4 mv = view * model;
+    glm::vec4 transformed = mv * position;
+
+    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(4);
+    std::cout << "Transformed vec is x=" << transformed.x
+              << " y=" << transformed.y
+              << " z=" << transformed.z
+              << "\n";
+    
+    return 0;
+}
